7b1e8d167357b6a8310cbefc336c3267d16bf0b7
athix123 <athizmubaroz2@gmail.com>
2019-10-01 13:32:21 +0700

    [WIP] redis queue

--- app/email.py
+++ app/email.py
@@ -7,11 +7,19 @@ def send_async_email(app, msg):
 	with app.app_context():
 		mail.send(msg)
 
-def send_email(subject, sender, recipients, text_body, html_body):
+
+def send_email(subject, sender, recipients, text_body, html_body, attachment=None, sync=False):
 	msg = Message(subject, sender=sender, recipients=recipients)
 	msg.body = text_body
 	msg.html = html_body
-	Thread(target=send_async_email, args=(app, msg)).start()
+	if attachments:
+		for attachment in attachments:
+			msg.attach(*attachment)
+	if sync:
+		mail.send(msg)
+	else:
+		Thread(target=send_async_email, args=(current_app._get_current_object(), msg)).start()
+
 
 def send_password_reset_email(user):
 	token = user.get_reset_password_token()
@@ -20,3 +28,4 @@ def send_password_reset_email(user):
 				recipients=[user.email],
 				text_body=render_template('email/reset_password.txt', user=user, token=token),
 				html_body=render_template('email/reset_password.html', user=user, token=token))
+	
